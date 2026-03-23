fcab79f7d140e294e5c868e58852ad40d55b3495
AbiaMaimun <abiamaimun@gmail.com>
2026-03-23 11:17:17 +0530

    initialEDA

--- Crop_Prediction.py
+++ Crop_Prediction.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
-cd=pd.read_csv('Crop Prediction dataset.csv')
+cd=pd.read_csv('crop_yield.csv')
 #Gives the number of rows and columns
 print(cd.shape)
 #Print's first 5 rows of dataset
@@ -13,9 +16,28 @@ print(cd.describe())
 #Checking for null values in each attribute
 print(cd.isnull().sum())
 #Filling the mean of production values at null spaces 
-cd['Production']=cd['Production'].fillna(cd['Production'].mean())
+cd['Production'] = cd['Production'].fillna(cd['Production'].mean())
+#df = cd.dropna(subset=['Production'])
 #Cross Verifying
 print(cd.isnull().values.any())
 # Yield = Production/Area
 cd['Yield']=(cd['Production']/cd['Area'])
 print(cd.head(10))
+
+
+sns.histplot(x=cd['Season'])
+plt.title("Histogram of seasons")
+plt.show() 
+
+sns.boxplot(x=cd["State"],y=cd["Crop_Year"])
+plt.show()
+
+data = cd.drop(['State','Season','Crop'], axis = 1)
+data.corr()
+sns.heatmap(data.corr(), annot =True, fmt='.4f')
+plt.title('Correlation Matrix')
+
+
+
+
+
